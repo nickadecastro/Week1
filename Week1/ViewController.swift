67deb0c794f31339ef8774c295b8cd255e9d27ae
nickadecastro <ndecastro@umass.edu>
2021-09-09 14:18:01 -0400

    part 2

--- Week1/ViewController.swift
+++ Week1/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func buttonShow(_ sender: UIButton) {
+        print("this button was clicked!")
+    }
+    
 
 }
 
